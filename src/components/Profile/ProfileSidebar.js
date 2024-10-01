abea190289f747d5bd656ec4b0134b0f03be5fd3
Bianca Benatti <bianca.benatti@bitize.com.br>
2024-10-01 11:28:06 -0300

    refactor: Separa logica do profile

--- src/components/Profile/ProfileSidebar.js
+++ src/components/Profile/ProfileSidebar.js
@@ -1,22 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { timeAgo } from '../../utils/timeUtils'; // Importando a função
 
 const ProfileSidebar = ({ userData }) => {
-  const timeAgo = (date) => {
-    const seconds = Math.floor((new Date() - new Date(date)) / 1000);
-    let interval = Math.floor(seconds / 31536000);
-    if (interval > 1) return `${interval} years ago`;
-    interval = Math.floor(seconds / 2592000);
-    if (interval > 1) return `${interval} months ago`;
-    interval = Math.floor(seconds / 86400);
-    if (interval > 1) return `${interval} days ago`;
-    interval = Math.floor(seconds / 3600);
-    if (interval > 1) return `${interval} hours ago`;
-    interval = Math.floor(seconds / 60);
-    if (interval > 1) return `${interval} minutes ago`;
-    return 'just now';
-  };
-
   return (
     <div className="profile-sidebar">
       <img src={userData.avatar_url} alt={userData.name} />
