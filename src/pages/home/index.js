0da566e150ab219adb6d5f4e3862dbb1187979c2
Bianca Benatti <bianca.benatti@bitize.com.br>
2024-10-01 10:47:04 -0300

    feat: Adiciona em pages profile

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './style.css';
+import './styles.css';
 
 const Home = () => {
   const [username, setUsername] = useState('');
