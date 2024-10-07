6c156969986d14aa351d008f7f73237bad817ad8
Bianca Benatti <bianca.benatti@bitize.com.br>
2024-10-07 16:26:38 -0300

    feat: Adiciona icons no ProfileSidebar

--- src/components/Profile/ProfileSidebar.js
+++ src/components/Profile/ProfileSidebar.js
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FaRegHeart, FaRegStar } from "react-icons/fa";
-import { IoPeopleOutline } from 'react-icons/io5';
+import { IoLocationOutline, IoPeopleOutline } from 'react-icons/io5';
+import { GoOrganization } from "react-icons/go";
+import { MdOutlineAlternateEmail } from 'react-icons/md';
+import { CgWebsite } from 'react-icons/cg';
+import { BsTwitterX } from 'react-icons/bs';
 
 const ProfileSidebar = ({ userData, username }) => {
   const [totalStars, setTotalStars] = useState(0);
@@ -27,7 +31,7 @@ const ProfileSidebar = ({ userData, username }) => {
 
   return (
     <div className="bg-[#262829] h-full w-full md:w-full text-slate-100 p-4 flex flex-col items-center border-r-gray-700 text-lg">
-      <img className="w-32 h-32 md:w-40 md:h-40 mb-4 mt-10 rounded-lg" src={userData.avatar_url} alt={userData.name} />
+      <img className="w-32 h-32 md:w-48 md:h-48 mb-4 mt-10 rounded-lg" src={userData.avatar_url} alt={userData.name} />
       <h2 className="mb-8 text-xl md:text-4xl mt-6 font-bold">{userData.name}</h2>
       <h3 className="font-light">{`@${username}`}</h3>
       {userData.bio && <p className="font-light text-center md:text-left">{userData.bio}</p>}
@@ -38,19 +42,19 @@ const ProfileSidebar = ({ userData, username }) => {
           <span className="flex gap-2 items-center"><FaRegStar /> {totalStars} stars</span>
         </div>
         <div className="mt-5 font-light md:text-left">
-          <p><strong>Organização:</strong> {userData.company || 'N/A'}</p>
-          <p><strong>Localização:</strong> {userData.location || 'N/A'}</p>
-          <p><strong>Email:</strong> {userData.email || 'N/A'}</p>
+          <p class="flex gap-2"><strong class="flex gap-2 items-center"><GoOrganization />Organização:</strong> {userData.company || 'N/A'}</p>
+          <p class="flex gap-2"><strong class="flex gap-2 items-center">< IoLocationOutline />Localização:</strong> {userData.location || 'N/A'}</p>
+          <p class="flex gap-2"><strong class="flex gap-2 items-center"><MdOutlineAlternateEmail />Email:</strong> {userData.email || 'N/A'}</p>
           {userData.blog && (
-            <p>
-              <strong>Website: </strong>
+            <p class="flex gap-2">
+              <strong class="flex gap-2 items-center"><CgWebsite />Website: </strong>
               <a href={userData.blog} target="_blank" rel="noopener noreferrer">{userData.blog}</a>
             </p>
           )}
-          {userData.linkedin && (
-            <p>
-              <strong>LinkedIn:</strong>
-              <a href={userData.linkedin} target="_blank" rel="noopener noreferrer">{userData.linkedin}</a>
+          {userData.twitter_username && (
+            <p class="flex gap-2">
+              <strong class="flex gap-2 items-center"><BsTwitterX />Twitter:</strong>
+              <a href={userData.twitter_username} target="_blank" rel="noopener noreferrer">@{userData.twitter_username}</a>
             </p>
           )}
         </div>
--- src/components/Profile/RepoItem.js
+++ src/components/Profile/RepoItem.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { timeAgo } from '../../utils/timeUtils'; 
 import { FaRegStar } from "react-icons/fa";
+import { MdOutlineUpdate } from "react-icons/md";
 
 const RepoItem = ({ repo }) => {
   return (
     <li className="mb-4 p-4 rounded-lg flex flex-col justify-start backdrop-blur-3xl border-t-gray-700 border-t-2 shadow-2xl">
       <a className="text-slate-100 no-underline font-bold text-lg md:text-2xl hover:underline" href={repo.html_url} target="_blank" rel="noopener noreferrer">{repo.name}</a>
       <p className="text-sm md:text-base">{repo.description || 'Sem descrição'}</p>
-      <div className="flex gap-1 mt-5 text-base">
-        <span className="flex gap-2 items-center"><FaRegStar className="text-sm"/> {repo.stargazers_count} -</span>
-        <span className="flex gap-1 items-center">Update {timeAgo(repo.updated_at)}</span>
+      <div className="flex gap-3 mt-5 text-base">
+        <span className="flex gap-1 items-center"><FaRegStar className="mb-1"/> {repo.stargazers_count}</span>
+        <span className="flex gap-1 items-center"> <MdOutlineUpdate />Update {timeAgo(repo.updated_at)}</span>
       </div>
     </li>
   );
